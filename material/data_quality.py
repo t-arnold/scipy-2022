10a9356b741930476e23c06463cbb9b9b03b40bb
Thomas Arnold <thomas.l.arnold@erdc.dren.mil>
2022-07-11 10:37:18 -0500

    breaking changes

--- material/data_quality.py
+++ material/data_quality.py
@@ -3,5 +3,5 @@ import pandas as pd
 def no_nas(product):
     df = pd.read_parquet(product['df'])
     
-    raise ValueError('oh noooo')
+    raise ValueError('Things are broken here')
     assert not df.MedHouseVal.isna().sum()
